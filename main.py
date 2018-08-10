101d21c8fcbee25bc1149e7161839277437fea9e
phantom5 <rickrudra@gmail.com>
2018-08-10 22:55:01 +0530

    pass hashin

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-#TODO : <RAJESH + AISURYA> GUI AND TAKING INPUT
-#TODO : <ALISHA> BCRYPT + PLUGIN + LINUX SCRIPT
-#TODO : <SASWAT> OTP VERIFICATION ROUTINE
-#TODO : <AISURYA> DEPLYOING FOR WINDOWS AND LINUX
-#TODO : <RUDRA> ENCRYPTION AND DATA STORAGE
+import hashlib
+
+def hashin_method(passwd):
+    hash_o=hashlib.sha3_512(passwd)
+    print(hash_o.hexdigest())
+    f=open('hakuna.txt','w')
+    f.write(hash_o.hexdigest())
+    f.close()
+
+
+res=input('signup or login')
+if res == 'signup':
+    passwd=input('enter password').encode('utf-8')
+    hashin_method(passwd)
+else:
+    hash_n=hashlib.sha3_512(input('enter your password').encode('utf-8'))
+    f=open('hakuna.txt','r')
+    if f.read() == hash_n.hexdigest():
+        print('You are in')
+    else:
+        print('Failed')
+
